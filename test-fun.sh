f3e4d7cd268d658bbfe4c333c0808f906fa811d7
seldamat <seldamat@gmail.com>
2017-04-24 14:40:32 -0400

    added run and cleanup functions for ssh-agent

--- test-fun.sh
+++ test-fun.sh
@@ -2,28 +2,27 @@
 # Define Function
 ## This function displays options to user and asks for verification
 function confirm_input(){
-confirm_user_input=$(cat <<EOF
-\n
-Confirm Input Arguments\n
-\n
-\t    HOST            ::      $host\n
-\t    USERNAME        ::      $username\n
-\n
-\t    SSH_HOME        ::      $ssh_home\n
-\t    SSHAGENTFILE    ::      $sshagentfile
-\n
-\t    KEYTYPE         ::      $keytype\n
-\t    NUM BITS        ::      $bits\n
-\n
-\t    PRIVATEKEY      ::      $privkey\n
-\t    PUBLICKEY       ::      $pubkey\n
-\n    
-\t    X11 AUTH?       ::      $x11\n
-
+    local confirm_user_input=$(cat <<-EOF
+    \n
+    📌 Confirm Input Arguments\n
+    \n
+    \t    🖥 HOST            ::      $host\n
+    \t    👤 USERNAME        ::      $username\n
+    \n
+    \t    📁 SSH_HOME        ::      $ssh_home\n
+    \t    📝 SSHAGENTFILE    ::      $sshagentfile
+    \n
+    \t    🔐 KEYTYPE         ::      $keytype\n
+    \n
+    \t    🔑 PRIVATEKEY      ::      $privkey\n
+    \t    🗝 PUBLICKEY       ::      $pubkey\n
+    \n    
+    \t    ⚙  X11 AUTH?       ::      $x11\n
 EOF
-)
-read -p "Proceed? [Y/N]" proceed
+    )   
+echo -e $confirm_user_input
+read -p "Proceed? [Y/N]  :: " proceed
 }
 
 confirm_input
-echo -e $confirm_user_input
+
