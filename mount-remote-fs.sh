ec53a34d93b0d2ac258b2f7349ec12e3ae27d71c
Shady <seldamat@gmail.com>
2017-05-02 19:51:24 -0400

    anchoring and zombie

--- mount-remote-fs.sh
+++ mount-remote-fs.sh
@@ -107,10 +107,10 @@ done
 ##
 # Define Function
 ## Assemble input arguments to final formats
-function mountremotefs_mkdir(){
+function mountremotefs_setup(){
 	mount_here=${mount_here}/${hostalias}
-	echo -e "\n  🗄  Setting up Local Directory to Host Remote File: $mount_here\n"
-	mkdir -p $mount_here
+	echo -e "\n  🗄  Setting up Local Directory to Host Remote File: $mount_here\n" && [ ! -d ${mount_here} ] && mkdir -p ${mount_here}
+	echo -e "\n ⚙  Creating Local Configuration Files\n" && [ ! -e "${HOME}/.ssh/.autosshfs" ] && mkdir -p ${HOME}/.ssh/.autosshfs
 }
 # Define Function
 ## Establish Persistent VPN Connection via OpenConnect
@@ -123,7 +123,7 @@ function mountremotefs_checksshkeys(){
 	# get location of current key
 	currentkey=$(ssh-add -L | awk '{print $3}')
 	if [ ! -z $currentkey ]; then
-		echo -e "\n  🔎 🔑  Found Existing Key Encryption For Automatic Login ($currentkey)"
+		echo -e "\n  🔎 🔑  Found Existing Key Encryption For Automatic Login ($currentkey)\n"
 	else
 		if [ -z $(type start_keychain) ]; then
 			echo -e "\n ❗️❗️  No Active Encryption Key Detected! Would you like to generate ssh keys for password-less login?\n [Y/N]"
@@ -137,17 +137,50 @@ function mountremotefs_checksshkeys(){
 			fi
 		else
 			start_keychain
+			read -p "**  Enter Path to Private Key to Enable Automatic Logins (~/.ssh/id_rsa)" private_key
+			if [ -z ${private_key} ]; then
+				private_key="~/.ssh/id_rsa"
+			else
+				[ ! -e ${private_key} ] && echo -e "\n ❗️❗️  WARNING - ${private_key} not found, sshfs will require password entry for each reconnection.. exiting....\n" && exit
+				[ -e ${private_key} ] && echo "\n\n ** Adding Existing Key to Keychain\n" && ssh-add ${private_key}
+			fi
 		fi
 	fi
 }
+# Define Function
+## Remove Zombification Signal & Anchor 
+function mountremotefs_banish(){
+	echo -e "\n\n 👹  Banishing SSHFS Zombie - Unmounting Remote File System, Deleting Zombie Signal\n"
+	rm ${HOME}/.ssh/.autosshfs/${hostalias}.zombie
+	umount -v ${mount_here}
+	## Add something here to check if umount failed
+		#[ "${mount_this}" = "/"] && ssh ${user}@${host} "rm ${mount_this}tmp/$(whoami).$(hostname).sshfs.anchor"
+		#[ "${mount_this}" != "/" ] && ssh ${user}@${host} "rm ${mount_this}/$(whoami).$(hostname).sshfs.anchor"
+	exit	
+}
+# Define Function
+## Create Anchor To Confirm Working Connection
+function mountremotefs_anchor(){
+	# Create the anchor in the home directory
+	echo -e "\n\n ⚓️  Anchoring Remote File System to Local Host\n"
+	local HOSTHOME=$(ssh ${user}@${host} 'echo ${HOME}')
+	ssh ${user}@${host} "mkdir -p .ssh/.autosshfs; touch ${HOSTHOME}/.ssh/.autosshfs/$(whoami).$(hostname).${hostalias}.anchor"
+	[ "${mount_this}" = "/"] && anchor="${mount_this}tmp/$(whoami).$(hostname).${hostalias}.autosshfs.anchor" && ssh ${user}@${host} "ln -s ${HOSTHOME}/.ssh/.autosshfs/$(whoami).$(hostname).${hostalias}.anchor ${anchor}"
+	[ "${mount_this}" != "/" ] && anchor="${mount_this}/$(whoami).$(hostname).${hostalias}.autosshfs.anchor" && ssh ${user}@${host} "ln -s ${HOSTHOME}/.ssh/.autosshfs/$(whoami).$(hostname).${hostalias}.anchor ${anchor}"
+}
 # Define Function 
-##
-function mountremotefs_sshfs(){
-sshfs ${user}@${host}:$mount_this $mount_here -C -o Ciphers=arcfour,cache=yes,kernel_cache,defer_permissions,reconnect,follow_symlinks,password_stdin	
+## Loop indefinitely while Zombie Signal Exists & Check for Anchor
+function mountremotefs_zombie(){
+	while [ -e ${HOME}./ssh/.autosshfs/${hostalias}.zombie ]; do
+		if [ ! -e ${mount_here}/tmp/$(whoami).$(hostname).${hostalias}.autosshfs.anchor] || [ ! -e ${mount_here}/$(whoami).$(hostname).${hostalias}.autosshfs.anchor]; then
+			echo -e "\n\nMissing Anchor - Resurrecting SSHFS Connection - $(date)\n\n"
+			$sshfs ${user}@${host}:$mount_this $mount_here -C -o Ciphers=arcfour,cache=yes,kernel_cache,defer_permissions,reconnect,follow_symlinks	
+		fi
+	done
 }
 
 function mountremotefs_run(){
-	mountremotefs_prepargs && mountremotefs_checkvpn && mountremotefs_checksshkeys && mountremotefs_sshfs
+	mountremotefs_mkdir && mountremotefs_checkvpn && mountremotefs_checksshkeys && mountremotefs_sshfs
 }
 
-mount
+open ${mount_here}
